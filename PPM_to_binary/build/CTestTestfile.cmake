9f23ce3bab08b9ab6a527844e4b2a2d8d5421f93
Frityet <ambhogal01@gmail.com>
2021-11-23 13:33:56 -0800

    gamer

--- PPM_to_binary/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Frityet/Documents/ComSciPrinciples/PPM_to_binary
-# Build directory: /Users/Frityet/Documents/ComSciPrinciples/PPM_to_binary/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
